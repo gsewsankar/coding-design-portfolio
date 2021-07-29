f52d58e266f263f16a2b82c69ca86048c32419f5
Garrick Sewsankar <gsewsankar@ufl.edu>
2021-07-29 19:03:47 -0400

    back to top button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,34 @@
 </svelte:head>
 
 <script>
+import { onMount } from 'svelte';
 import Skills from './skills.svelte';
 import Experience from './experience.svelte';
 import Projects from './projects.svelte';
 import Education from './education.svelte';
 import Contact from './contact.svelte';
+
+let mybutton;
+
+onMount(() => {
+	mybutton = document.getElementById("myBtn");
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			mybutton.style.display = "block";
+		} else {
+			mybutton.style.display = "none";
+		}
+		}
+	});
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+
 </script>
 
 <style>
@@ -47,6 +70,26 @@ import Contact from './contact.svelte';
 
 	h3{
 		padding: 1rem;
+	}
+
+	.scrollUp {
+		display: none;
+		position: fixed;
+		bottom: 20px;
+		right: 30px;
+		z-index: 99;
+		font-size: 18px;
+		border: none;
+		outline: none;
+		background-color: rgba(109, 109, 109, 0.829);
+		color: white;
+		cursor: pointer;
+		padding: 15px;
+		border-radius: 4px;
+	}
+
+	.scrollUp:hover {
+		background-color:rgba(143, 143, 143, 0.89);
 	}
 
 	@keyframes floating {
@@ -93,4 +136,5 @@ import Contact from './contact.svelte';
 <Projects/>
 <Education/>
 <Contact/>
+<button on:click={topFunction} id="myBtn" class="scrollUp" title="Go to top">Top</button>
 </div>
